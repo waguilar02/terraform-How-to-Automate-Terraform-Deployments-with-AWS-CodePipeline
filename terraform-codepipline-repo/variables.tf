01ca328e6ce0a91592cd0665338672b202ca08eb
waguilar02 <67397337+waguilar02@users.noreply.github.com>
2024-10-10 19:18:47 -0500

    Update variables.tf
    default talentotech-will

--- terraform-codepipline-repo/variables.tf
+++ terraform-codepipline-repo/variables.tf
@@ -15,7 +15,7 @@ variable "key_name" {
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for remote state"
-  default = "terraform-codepipline-bucket"
+  default = "talentotech-will"
 }
 
 variable "name" {
